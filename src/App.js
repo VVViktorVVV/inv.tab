235829336db63976f2134e60c884ee5166aeffac
VVViktorVVV <86729213+VVViktorVVV@users.noreply.github.com>
2022-11-18 22:27:13 +0200

    2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 // import Counter from "./componens/Counter";
 // import ClassCounter from "./componens/classCounter";
 import "./styles/App.css";
 import Post from "./componens/Post";
 function App() {
+  const [posts, setPost] = useState(
+    [
+      { id: 1, title: "firstEL", body: "content about firstEL" },
+      { id: 1, title: "firstEL", body: "content about firstEL" },
+      { id: 1, title: "firstEL", body: "content about firstEL" }
+    ]
+  )
 
 
-  // let [value, setValue] = useState("TEXT");
-
-
-
-
-  
+   
   return (
     <div className="App">
       {/* <Counter />
       <ClassCounter /> */}
-      <Post />
+
+      
+      <Post post={{ id: 1, title: "firstEL", body: "content about firstEL" }} />
 
          
       
--- src/componens/Post.jsx
+++ src/componens/Post.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-const Post = function () {
-    
+const Post = function (props) {
+  console.log(props);
+
     return (
     <div className="post">
-        <h1 className="post__title">Thing Name</h1>
-        <p className="post__info">About the thing</p>
+        <h1 className="post__title">{props.post.title}</h1>
+        <p className="post__info">{props.post.id}</p>
+        <p className="post__info">{props.post.body}</p>
         <button type="button" className="post__button">Delete</button>
       </div>
     )
