1b7ebfe63aacc2cbea4d4e19a48b7c1a579c68ff
VVViktorVVV <86729213+VVViktorVVV@users.noreply.github.com>
2022-11-22 19:54:55 +0200

    post styles

--- src/App.js
+++ src/App.js
@@ -12,22 +12,49 @@ function App() {
   const [posts, setPosts] = useState(
     [
       { id: 1, title: "firstEL", body: "content about firstEL" },
-      { id: 2, title: "secondEL", body: "content about firstEL" },
-      { id: 3, title: "thirdEL", body: "content about firstEL" },
-      { id: 4, title: "firstEL", body: "content about firstEL" },
-      { id: 5, title: "secondEL", body: "content about firstEL" },
-      { id: 6, title: "thirdEL", body: "content about firstEL" }
+      { id: 2, title: "secondEL", body: "content about firstEL" }
+      // { id: 3, title: "thirdEL", body: "content about firstEL" },
+      // { id: 4, title: "firstEL", body: "content about firstEL" },
+      // { id: 5, title: "secondEL", body: "content about firstEL" },
+      // { id: 6, title: "thirdEL", body: "content about firstEL" }
     ]
-  )
+  );
+
+  const [title, setTitle] = useState();
+  const [body, setBody] = useState(); 
+
+  const addNewPost = function (e) {
+    e.preventDefault();
+
+    const newPost = {
+      id: Date.now(),
+      title,
+      body
+    };
+
+    setPosts ([...posts, newPost])
+    
+    console.log(body);
+  }
 
 
    
   return (
     <div className="App">
       <form>
-        <MyInput type="text" placeholder="Name post"/>
-        <MyInput type="text" placeholder="About post"/>
-        <MyButton>Creat post</MyButton>
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder="Name post"
+        />
+        <MyInput
+          value={body}
+          onChange={e => setBody(e.target.value)}
+          type="text"
+          placeholder="About post"
+        />
+        <MyButton onClick={addNewPost}>Creat post</MyButton>
       </form>
       <PostList newPost={posts} title="The best post List" />
          
